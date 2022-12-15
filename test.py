bcfd1ca000a5c639ccb42f019454182e40f2fc48
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2022-12-15 16:11:25 +0200

    new untitled

--- test.py
+++ test.py
@@ -5,15 +5,15 @@ from bubble_sheet_answer import *
 import glob
 
 
-# for name in glob.glob('./testCases/*'):
-#     print(name)
-#     image= cv2.imread(name) 
+for name in glob.glob('./testCases/*'):
+    print(name)
+    image= cv2.imread(name) 
 
-#     paper=extract_the_paper_from_image(image)
+    paper=extract_the_paper_from_image(image)
 
-#     answers=get_student_answer(paper,200,(15,50))
+    answers=get_student_answer(paper,200,(15,50))
 
-#     print(answers,len(answers))
+    print(answers,len(answers))
 
 image= cv2.imread('omr.jpg') 
 paper=extract_the_paper_from_image(image)
